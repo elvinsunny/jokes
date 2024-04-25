8f46458917d14f32c2fa16260d470b64aec14579
elvinsunny <118191671+elvinsunny@users.noreply.github.com>
2024-04-25 18:21:37 +0530

    Update urls.py

--- project/urls.py
+++ project/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include  
+from django.urls import path, include 
+from django.shortcuts import redirect
 
 def redirect_to_jokes(request):
     return redirect('joke-list-create')# Import include for including app URLs
